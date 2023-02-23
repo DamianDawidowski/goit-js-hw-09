3a7ae12680fd654c6d9b54d1bfc316e3a3052dc8
DamianDawidowski <damiandawidowski@gmail.com>
2023-02-23 22:41:33 +0000

    Deploying to gh-pages from @ DamianDawidowski/goit-js-hw-09@b8fcc2feb318e49e836d1023152522d2f067d032 🚀

--- 01-color-switcher.3cf7026b.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("body"),n=document.querySelector("button"),t=n.nextElementSibling,d=null;t.disabled=!0,n.addEventListener("click",(function(){d=setInterval((function(){var n;n="#".concat(Math.floor(16777215*Math.random()).toString(16)),e.style.backgroundColor=n}),1e3),n.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(d),n.disabled=!1,t.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.3cf7026b.js.map
---
+++ 01-color-switcher.6b04c2e4.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body"),n=document.querySelector("button"),t=n.nextElementSibling,d=null;t.disabled=!0,n.addEventListener("click",(function(){d=setInterval((function(){var n;n="#".concat(Math.floor(16777215*Math.random()).toString(16)),e.style.backgroundColor=n}),1e3),n.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(d),n.disabled=!1,t.disabled=!0})),XXXXXXXXXXX}();
+//# sourceMappingURL=01-color-switcher.6b04c2e4.js.map
